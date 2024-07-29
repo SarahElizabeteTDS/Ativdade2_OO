358ccffc345607941a37d72f7635e901ac696b05
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-07-28 23:29:33 -0300

    Update att2.php

--- att2.php
+++ att2.php
@@ -5,27 +5,25 @@ class Pokemon
     //setando atributos
     public $nome;
     public $tipos;  
+    public $forca;
     public $fraquezas; 
     public $peso;
     public $altura;
     public $vida; 
-    public $ataque;
     public $nivel; 
-    public $defesa;
     public $evolucao;
 
     //setando construtor
-    function __construct($nome, $tipos, $counter, $peso, $alt, $vida, $atq, $lvl, $def, $evo)
+    function __construct($nome, $tipos, $counter, $peso, $alt, $vida, $forca, $lvl, $evo)
     {
         $this->nome = $nome;
         $this->tipos = $tipos;
+        $this->forca = $forca;
         $this->fraquezas = $counter;
         $this->peso = $peso;
         $this->altura = $alt;
         $this->vida = $vida;
-        $this->ataque = $atq;
         $this->nivel = $lvl;
-        $this->defesa = $def;
         $this->evolucao = $evo;
     }
 
@@ -35,6 +33,50 @@ class Pokemon
     }
 
     function atacar(){
-        //terminar em casa
+        $var = rand(1, 10);
+        print "Você atacou e deu " . $var . " de dano.\n\n";
+        $this -> vida -= $var;
+    }
+
+    function curar(){
+        $var = rand(1, 10);
+        print "Você se curou e recebeu " . $var . " de vida.\n\n";
+        $this -> vida += $var;
+    }
+
+    function batalhar(){
+        $var = rand(1, 10);
+        print "Você está em uma luta, se prepare!\n";
+        $escolha = readline("Você escolhe: \n1.ATACAR\n2.CURAR\n");
+        if ($escolha == 1) 
+        {
+            print "Você atacou e deu " . $var . " de dano.\n\n";
+            $this -> vida -= $var;
+        }else if($escolha == 2){
+            print "Você se curou e recebeu " . $var . " de vida.\n\n";
+            $this -> vida += $var;
+        }else{
+            print "Opcao Invalida. Insira novamente\n";
+            $escolha = readline("Você escolhe: \n1.ATACAR\n2.CURAR\n");
+        }
+    }
+
+    function imprimirDados(){
+        print"\n\nO Pokémon escolhido é o " . $this ->nome . "\nSeus tipos são: " . $this ->tipos . "\nSeus counters são: " . $this ->fraquezas . "\nPesa cerca de: " . $this ->peso . "kg". "\nTem: " . $this ->altura . " centímetros de altura\nPossui: " . $this ->vida . " de vida\n" . $this ->forca . " de forca\n" . "Level atual: " . $this ->nivel . "\nSua evolucao anterior é o: " . $this ->evolucao . "\n\n";
     }
 }
+
+$pokemon1 = new Pokemon("Lucario", "Lutador e Metal", "Fogo, Lutador e Terrestre", 54, 120, 100, 50, 0, "Riolu");
+$pokemon2 = new Pokemon("Zoroark", "Dark", "Lutador, Inseto e Fada", 81, 160, 100, 50, 0, "Zorua");
+
+$pokemon1->imprimirDados();
+$pokemon1->atacar();
+$pokemon1->curar();
+$pokemon1->batalhar();
+$pokemon1->upar();
+
+$pokemon2->imprimirDados();
+$pokemon2->atacar();
+$pokemon2->curar();
+$pokemon2->batalhar();
+$pokemon2->upar();
